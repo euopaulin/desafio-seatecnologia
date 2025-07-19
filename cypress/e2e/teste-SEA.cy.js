e48bd7a39fc4c65eb7b9da8108c9be6a19fc7fb3
Paulo Henrique <157616381+euopaulin@users.noreply.github.com>
2025-07-19 10:00:11 -0300

    Up

---
+++ cypress/e2e/teste-SEA.cy.js
@@ -0,0 +1,24 @@
+describe('Teste de cadastro de novo usuário', () => {
+  it('Deve cadastrar um novo usuário', () => {
+    // Acessa a página de cadastro de novo usuário
+    cy.visit('https://analista-teste.seatecnologia.com.br/')
+
+    //localiza o botão e clica nele
+    cy.get('button.c-kUQtTK').click();
+
+    //altera o botão de inativo para ativo
+    cy.get('button.ant-switch').click()
+
+    //Preenche o campo de nome
+    cy.get('input[name="name"]').type('Paulo Henrique');
+
+    //Preenche o campo do CPF
+    cy.get('input[name="cpf"]').type('cpfnumeros');
+
+    // Verifica se o campo CPF foi preenchido corretamente
+     cy.get('input[name="cpf"]').should('have.value', 'cpfnumeros');
+
+     //Verifica o campo do RG
+     cy.get('input[name="rg"]').type('123456789121212131313142312412312312313431513513123');
+    });
+  });
