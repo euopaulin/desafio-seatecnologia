343037aa4d071d02407b1dcfffe5440e0ba14ea4
Paulin <paulohspfc209@gmail.com>
2025-07-21 23:29:22 -0300

    update

--- cypress/e2e/teste-SEA.cy.js
+++ cypress/e2e/teste-SEA.cy.js
@@ -11,7 +11,7 @@ describe('Teste da página de cadastro de novos funcionarios', () => {
 
     //Esse comando vai verificar se o status do botão está ativo
     cy.get('button.ant-switch').then(($status) => {
-        if ($status.text() === 'Ativo') {
+        if ($status.text() == 'ativo') {
         cy.log('O status está ativo');
         } else {
         cy.log('O status não está ativo');
@@ -19,7 +19,7 @@ describe('Teste da página de cadastro de novos funcionarios', () => {
     });
 
     //Alterar botão de radio para o sexo masculino
-    cy.get('button.ant-radio ant-radio-checked').click();
+    cy.get('input[value="feminino"]').click();
 
     // Preencher o campo de nome
     cy.get('input[name="name"]').type('Paulo Henrique');
@@ -28,12 +28,12 @@ describe('Teste da página de cadastro de novos funcionarios', () => {
     cy.get('input[name="cpf"]').type('12345678901');
 
     // Verificar se o campo CPF foi preenchido corretamente
-     cy.get('input[name="cpf"]').should('have.value', '12345678901');
+    cy.get('input[name="cpf"]').should('have.value', '12345678901');
 
-     // Verificar o campo do RG
-     cy.get('input[name="rg"]').type('123456789121212131313142312412312312313431513513123');
-
-     // Verificar o campo de Adicionar EPI
-     cy.get('span.addEPI').click();
+    // Verificar o campo do RG
+    cy.get('input[name="rg"]').type('123456789121212131313142312412312312313431513513123');
+  
+    // Verificar o campo de Adicionar EPI
+    cy.get('span.addEPI').click();
     });
   });
