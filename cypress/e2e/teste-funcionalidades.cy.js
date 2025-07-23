6aae3a04519cb96cd5ff5ee8144ee6f91938cfc1
Paulo Henrique <paulohspfc209@gmail.com>
2025-07-23 13:21:32 -0300

    update

--- cypress/e2e/teste-funcionalidades.cy.js
+++ cypress/e2e/teste-funcionalidades.cy.js
@@ -1,27 +1,22 @@
 describe('Teste da página de cadastro de novos funcionarios', () => {
   it('Cadastrar um novo funcionario', () => {
-    // Para acessar o site:
+    
     cy.visit('https://analista-teste.seatecnologia.com.br/')
 
-    // localizar o botão e clicar nele
+    // localizar o botão de "Cadastrar novo funcionario" clicar nele
     cy.get('button.c-kUQtTK').click();
 
-    //Alterar botão de radio para o sexo masculino
-    cy.get('input[value="feminino"]').click();
+    //Os comandos abaixo vão preencher os campos do formulário de cadastro de novo funcionário
+    cy.get('input[value="fMasculino"]').click();
 
-    // Preencher o campo de nome
     cy.get('input[name="name"]').type('Paulo Henrique');
 
-    // Preencher o campo do CPF
     cy.get('input[name="cpf"]').type('12345678901');
 
-    // Verificar se o campo CPF foi preenchido corretamente
     cy.get('input[name="cpf"]').should('have.value', '12345678901');
 
-    // Verificar o campo do RG
     cy.get('input[name="rg"]').type('123456789121212131313142312412312312313431513513123');
   
-    //Preencher o campo de data de nascimento
     cy.get('input[name="birthDay"]').type('2000-08-14');
 
     // Alterar o botão de inativo para ativo
@@ -38,7 +33,6 @@ describe('Teste da página de cadastro de novos funcionarios', () => {
 
     // Verificar o campo de Adicionar EPI
     cy.get('span.addEPI').click();
-
 
     });
   });
